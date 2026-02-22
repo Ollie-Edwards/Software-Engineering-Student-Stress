b4287b4d242b6cf9169f15c52fba6e0549eeead6
Ollie-Edwards <ollie3edwards@gmail.com>
2026-02-22 18:40:51 +0000

    Added changes to testing scripts, unit tests fail if coverage is under 95%

--- run_testing.bat
+++ run_testing.bat
@@ -3,7 +3,7 @@ echo Starting backend tests...
 
 REM === Install dependencies and run tests ===
 echo Installing dependencies and running tests...
-start cmd /k "cd /d Backend && python -m pip install --upgrade pip && pip install pytest pytest-cov httpx && pip install -r requirements.txt && pytest --cov=app --cov-report=term-missing --cov-report=html:htmlcov && start htmlcov\index.html"
+start cmd /k "cd /d Backend && python -m pip install --upgrade pip && pip install pytest pytest-cov httpx && pip install -r requirements.txt && pytest --cov=app --cov-report=term-missing --cov-report=html:htmlcov --cov-fail-under=95 && start htmlcov\index.html"
 
 echo Test process launched.
 pause

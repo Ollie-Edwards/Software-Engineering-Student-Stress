78671b00052d45a5bd730e4a0b9748cc920c7d1e
Ollie-Edwards <ollie3edwards@gmail.com>
2026-03-05 12:34:03 +0000

    (added) added batch file for automated test running, and fixed changed automated test running to produce html documents for inspection

--- Frontend/vitest.config.js
+++ Frontend/vitest.config.js
@@ -3,9 +3,14 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./setupTests.js'],
+    reporter: ['verbose'],
+    outputFile: './html/index.html',
     coverage: {
       provider: 'v8',
       reporter: ['html', 'text-summary'],
+      reportsDirectory: './coverage',
       include: ['src/**/*.{js,ts,jsx,tsx}'],
       exclude: [
         'src/**/*.test.{js,ts,jsx,tsx}',

9649db740c2258121ed5eeee9a509cc32dd5d6ed
Ollie Edwards <ollie3edwards@gmail.com>
2026-03-06 11:48:35 +0000

    (fix)

--- Backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from models.user import User
-from models.task import Task
-from models.subtask import Subtask
-from models.reminders import Reminders
-from models.notification import Notification
--- Backend/app/models/notification.py
+++ Backend/app/models/notification.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Text, Boolean, TIMESTAMP, ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
-from ..database import Base
+from app.database import Base
 
 
 class Notification(Base):

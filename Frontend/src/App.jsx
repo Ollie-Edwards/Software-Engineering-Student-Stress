45d1492fe2fbaac367a943a67ec502c23e5a9486
Ollie-Edwards <ollie3edwards@gmail.com>
2026-03-09 10:52:40 +0000

    (refactor) prettify everything - added batch file for lint and formatting

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,35 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import Home from './pages/home';
-import NavigationBar from './components/NavigationBar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from "./pages/home";
+import NavigationBar from "./components/NavigationBar";
 import React, { useState } from "react";
-import MoodleTasks from './MoodleTask';
+import MoodleTasks from "./MoodleTask";
 
 function App() {
   const [isAdding, setIsAdding] = useState(false);
   return (
     <div className="min-h-screen bg-gray-50">
       <BrowserRouter>
-      <NavigationBar onAddTask={() => setIsAdding(true)} />
+        <NavigationBar onAddTask={() => setIsAdding(true)} />
         <Routes>
-          <Route path="/" element={<Home isAdding={isAdding} setIsAdding={setIsAdding} />} /> 
-          <Route path="/home" element={<Home isAdding={isAdding} setIsAdding={setIsAdding} />} />
+          <Route
+            path="/"
+            element={<Home isAdding={isAdding} setIsAdding={setIsAdding} />}
+          />
+          <Route
+            path="/home"
+            element={<Home isAdding={isAdding} setIsAdding={setIsAdding} />}
+          />
           <Route path="/about" element={<h1>About</h1>} />
           <Route path="/moodle" element={<MoodleTasks />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
